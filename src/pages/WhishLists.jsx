74dff2950ac88b6cc6fb5ecc037596c8908260dc
Iftikhar-hub <iftikharifti1673@gmail.com>
2026-01-14 21:35:19 +0500

    Completed Add To Cart Page

--- src/pages/WhishLists.jsx
+++ src/pages/WhishLists.jsx
@@ -8,7 +8,7 @@ import { ProductIcons } from '../utils/data.js'
 
 import Header from "../components/Header";
 import Navbar from "../components/Navbar";
-import Footer from "../components/footer";
+import Footer from "../components/footer.jsx";
 
 
 const WhishList = () => {
