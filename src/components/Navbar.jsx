e8e139f8bd0522ee2d18d06562c31d104d728253
Iftikhar-hub <iftikharifti1673@gmail.com>
2026-01-20 21:28:48 +0500

    User Profile Continue

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -62,8 +62,9 @@ const Navbar = ({ userId }) => {
                     </div>
 
                 </div>
-
-                <UserProfile />
+                {userData && (
+                    <UserProfile />
+                )}
 
             </div>
 
