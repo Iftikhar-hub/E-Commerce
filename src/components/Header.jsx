b25a7f9963d149b7567c5594b4dc83927125b677
Iftikhar-hub <iftikharifti1673@gmail.com>
2026-01-30 21:38:03 +0500

    Header Responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,17 +2,17 @@ import DropDown from '../assets/DropDown.svg'
 
 const Header = () => {
     return (
-        <div className="w-full pl-111 pr-34 py-3  bg-black flex items-center justify-center">
-            <div className="flex flex-row items-center justify-center gap-56">
+        <div className="w-full px-6  lg:pl-45 lg:pr-14 xl:pl-90 xl:pr-24 2xl:pl-111 2xl:pr-34 py-3  bg-black ">
+            <div className="flex flex-row items-center justify-between lg:justify-center lg:gap-56">
                 <div className="flex flex-row gap-2 items-center justify-center">
-                    <p className="font-poppins text-sm font-normal leading-5.25 tracking-normal text-[#FAFAFA]">
+                    <p className="font-poppins text-[12px] sm:text-sm font-normal leading-5.25 tracking-normal text-[#FAFAFA]">
                         Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!
                     </p>
-                    <a href="/" className="font-poppins text-sm font-semibold leading-[24px] text-center underline decoration-solid text-[#FAFAFA]">ShopNow</a>
+                    <a href="/" className="max-[548px]:hidden font-poppins text-[12px] sm:text-sm font-semibold leading-[24px] text-center underline decoration-solid text-[#FAFAFA]">ShopNow</a>
                 </div>
 
-                <div className="flex flex-row gap-1.25 ">
-                    <p className="font-poppins text-sm font-normal leading-5.25 tracking-normal text-[#FAFAFA]">English</p>
+                <div className="max-[490px]:hidden flex flex-row gap-1.25 items-center sm:items-start ">
+                    <p className="font-poppins text-[12px] sm:text-sm font-normal leading-5.25 tracking-normal text-[#FAFAFA]">English</p>
                     <img src={DropDown} alt="DropDown" className='w-6 h-6 cursor-pointer'/>
                 </div>
           </div>
