215eb59093a0ab113d05bc8b32eebd275a7aff45
Iftikhar-hub <iftikharifti1673@gmail.com>
2026-02-03 21:24:36 +0500

    User Cart Specific

--- src/components/userProfile.jsx
+++ src/components/userProfile.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { useGetUserDataQuery } from '../services/userApi';
 import { useNavigate } from 'react-router-dom';
 import axios from "axios";
+import { clearCart } from '../services/adToCart';
 import { useDispatch } from 'react-redux';
 const UserProfile = ({ userId }) => {
      const navigate = useNavigate();
