db916b55bd3cfa06ef2f2fd1a47958294563aeea
Iftikhar-hub <iftikharifti1673@gmail.com>
2026-02-03 18:36:34 +0500

    CORS Problem Resolved

--- src/components/userProfile.jsx
+++ src/components/userProfile.jsx
@@ -8,7 +8,7 @@ const UserProfile = ({ userId }) => {
      const handleLogout = async () => {
             try {
                 await axios.post(
-                    'http://e-commerce-backend-production-6436.up.railway.app/api/user/user-logout',
+                    'https://e-commerce-backend-production-6436.up.railway.app/api/user/user-logout',
                     {},
                     { withCredentials: true }
                 );
