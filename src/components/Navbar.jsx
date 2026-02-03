a88b3d1a4a4f9d4eefa8e468911ae8a075f56028
Iftikhar-hub <iftikharifti1673@gmail.com>
2026-02-03 18:35:31 +0500

    CORS Problem Resolved

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -46,7 +46,7 @@ const Navbar = ({ userId }) => {
     const handleLogout = async () => {
         try {
             await axios.post(
-                'http://e-commerce-backend-production-6436.up.railway.app/api/user/user-logout',
+                'https://e-commerce-backend-production-6436.up.railway.app/api/user/user-logout',
                 {},
                 { withCredentials: true }
             );
