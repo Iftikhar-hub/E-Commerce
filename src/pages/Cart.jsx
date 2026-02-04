9983bd8bfb7eb1d6bb394c20d1180805eeb86f9a
Iftikhar-hub <iftikharifti1673@gmail.com>
2026-02-04 13:23:13 +0500

    User Cart Specific

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -37,37 +37,38 @@ const Cart = () => {
                 <p className="text-[22px] text-red-900 font-bold text-center mt-6">Your cart is empty</p>
             ) : (
                 <div className="w-full max-w-400 px-36 flex flex-col gap-20 items-center justify-center mx-auto mt-20">
-                    <table className="w-full border-separate border-spacing-y-4">
+                        <table className="w-full border-separate border-spacing-y-4">
                         <thead>
-                            <tr className="bg-white/5 shadow-sm backdrop-blur-sm">
-                                <th>Product</th>
-                                <th>Price</th>
-                                <th>Quantity</th>
-                                <th>Subtotal</th>
-                                <th>Action</th>
+                            <tr className="bg-white/5   shadow-sm backdrop-blur-sm">
+                                    <th className="px-2 py-2 text-left font-poppins font-normal text-[18px] leading-6 text-black">Product</th>
+                                    <th className="px-2 py-2 text-left font-poppins font-normal text-[18px] leading-6 text-black">Price</th>
+                                    <th className="px-2 py-2 text-left font-poppins font-normal text-[18px] leading-6 text-black">Quantity</th>
+                                    <th className="px-2 py-2 text-left font-poppins font-normal text-[18px] leading-6 text-black">Subtotal</th>
+                                    <th className="px-2 py-2 text-left font-poppins font-normal text-[18px] leading-6 text-black">Action</th>
                             </tr>
                         </thead>
                         <tbody>
                             {cartItems.map((cart) => (
                                 <tr key={cart._id}>
-                                    <td>
+                                    <td className="px-2 py-2">
                                         <div className="flex items-center gap-3">
                                             <img src={cart.image} alt={cart.pname} className="w-13.5 h-13.5 object-cover" />
                                             <p>{cart.pname}</p>
                                         </div>
                                     </td>
-                                    <td>${cart.discountedPrice ?? cart.orignalPrice}</td>
+                                    <td className="px-2 py-2 font-poppins font-normal text-[16px] leading-6 text-black">${cart.discountedPrice ?? cart.orignalPrice}</td>
                                     <td>
                                         <input
                                             type="number"
                                             min={1}
                                             value={cart.quantity}
                                             onChange={(e) => handleQuantityChange(cart._id, e.target.value)}
+                                            className="border border-[#87878a] w-12 pl-2 rounded-sm"
                                         />
                                     </td>
                                     <td>${(cart.discountedPrice ?? cart.orignalPrice) * cart.quantity}</td>
                                     <td>
-                                        <button onClick={() => handleRemove(cart._id)}>Remove</button>
+                                        <button className="bg-red-600 text-white px-2 py-1 rounded-sm" onClick={() => handleRemove(cart._id)}>Remove</button>
                                     </td>
                                 </tr>
                             ))}
